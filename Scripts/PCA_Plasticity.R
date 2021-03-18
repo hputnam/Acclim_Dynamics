a00f6307c775af383adc562f3eed4327b7a0f3ca
Emma Strand <emma_strand@uri.edu>
2021-03-18 15:52:23 -0400

    adding segment lines to pca plots

--- Scripts/PCA_Plasticity.R
+++ Scripts/PCA_Plasticity.R
@@ -89,12 +89,29 @@ Mcapitata.all <- Mcap.pca.out %>%
   augment(Mcap.info) %>% # add original dataset back in
   group_by(Timepoint, Temperature) %>%
   mutate(PC1.mean = mean(.fittedPC1),
-         PC2.mean = mean(.fittedPC2))
+         PC2.mean = mean(.fittedPC2)) %>% select(-.fittedPC4, -.fittedPC5, -.fittedPC6)
+high <- Mcapitata.all %>% subset(Temperature == "High")
+amb <- Mcapitata.all %>% subset(Temperature == "Ambient")
+
+filter(Mcapitata.all, Timepoint == "Day 2" & Temperature == "Ambient")
+filter(Mcapitata.all, Timepoint == "2 week" & Temperature == "Ambient")
+filter(Mcapitata.all, Timepoint == "4 week" & Temperature == "Ambient")
+filter(Mcapitata.all, Timepoint == "8 week" & Temperature == "Ambient")
+filter(Mcapitata.all, Timepoint == "12 week" & Temperature == "Ambient")
+
+### come back to finding a more elegant way to do geom segment; this will have to change after I do protein  
 
 Mcapitata.all.fig <- ggplot(Mcapitata.all, aes(.fittedPC1, .fittedPC2, color = Temperature)) + 
   geom_point(size = 1.5, alpha=0.3) +
   geom_point(aes(x=PC1.mean, y=PC2.mean)) +
-  geom_line(aes(x=PC1.mean, y=PC2.mean)) +
+  geom_segment(aes(x = 1.06, y = 0.351, xend = -0.239, yend = -0.0732, colour = Temperature), data=high) + # Day 2 to 2 week
+  geom_segment(aes(x = -0.239, y = -0.0732, xend = 0.703, yend = -0.202, colour = Temperature), data=high) + # 2 week to 4 week
+  geom_segment(aes(x = 0.703, y = -0.202, xend = -1.48, yend = -0.281, colour = Temperature), data=high) + # 4 week to 8 week
+  geom_segment(aes(x = -1.48, y = -0.281, xend = -0.821, yend = 0.662, colour = Temperature), data=high, arrow = arrow()) + # 8 week to 12 week
+  geom_segment(aes(x = 0.121, y = 0.0600, xend = 0.572, yend = -0.0918, colour = Temperature), data=amb) + # Day 2 to 2 week
+  geom_segment(aes(x = 0.572, y = -0.0918, xend = 0.968, yend = -0.279, colour = Temperature), data=amb) + # 2 week to 4 week
+  geom_segment(aes(x = 0.968, y = -0.279, xend = -0.352, yend = -0.520, colour = Temperature), data=amb) + # 4 week to 8 week
+  geom_segment(aes(x = -0.352, y = -0.520, xend = -0.444, yend = 0.543, colour = Temperature), data=amb, arrow = arrow()) + # 8 week to 12 week
   geom_text(aes(PC1.mean, PC2.mean, label=Timepoint), vjust=-1.5, color="black") +
   theme_half_open(12) + background_grid() +
   ggtitle("Montipora capitata") + 
@@ -167,12 +184,28 @@ Pact.all <- Pact.pca.out %>%
   augment(Pact.info) %>% # add original dataset back in
   group_by(Timepoint, Temperature) %>%
   mutate(PC1.mean = mean(.fittedPC1),
-         PC2.mean = mean(.fittedPC2))
+         PC2.mean = mean(.fittedPC2)) %>% select(-.fittedPC4, -.fittedPC5, -.fittedPC6)
+
+high <- Pact.all %>% subset(Temperature == "High")
+amb <- Pact.all %>% subset(Temperature == "Ambient")
+
+filter(Pact.all, Timepoint == "Day 2" & Temperature == "High")
+filter(Pact.all, Timepoint == "2 week" & Temperature == "High")
+filter(Pact.all, Timepoint == "4 week" & Temperature == "High")
+filter(Pact.all, Timepoint == "8 week" & Temperature == "High")
+filter(Pact.all, Timepoint == "12 week" & Temperature == "High")
   
 Pact.all.fig <- ggplot(Pact.all, aes(.fittedPC1, .fittedPC2, color = Temperature)) + 
   geom_point(size = 1.5, alpha=0.3) +
   geom_point(aes(x=PC1.mean, y=PC2.mean)) +
-  geom_line(aes(x=PC1.mean, y=PC2.mean)) +
+  geom_segment(aes(x = -0.813, y = -1.03, xend = -1.30, yend = -0.0509, colour = Temperature), data=amb) + # Day 2 to 2 week
+  geom_segment(aes(x = -1.30, y = -0.0509, xend = 0.138, yend = -0.556, colour = Temperature), data=amb) + # 2 week to 4 week
+  geom_segment(aes(x = 0.138, y = -0.556, xend = -0.0865, yend = 0.463, colour = Temperature), data=amb) + # 4 week to 8 week
+  geom_segment(aes(x = -0.0865, y = 0.463, xend = -0.173, yend = 0.479, colour = Temperature), data=amb, arrow = arrow()) + # 8 week to 12 week
+  geom_segment(aes(x = -0.391, y = -0.616, xend = -0.0670, yend = 0.196, colour = Temperature), data=high) + # Day 2 to 2 week
+  geom_segment(aes(x = -0.0670, y = 0.196, xend = 0.545, yend = 0.233, colour = Temperature), data=high) + # 2 week to 4 week
+  geom_segment(aes(x = 0.545, y = 0.233, xend = 1.61, yend = 0.466, colour = Temperature), data=high) + # 4 week to 8 week
+  geom_segment(aes(x = 1.61, y = 0.466, xend = 3.52, yend = 0.746, colour = Temperature), data=high, arrow = arrow()) + # 8 week to 12 week
   geom_text(aes(PC1.mean, PC2.mean, label=Timepoint), vjust=-1.5, color="black") +
   theme_half_open(12) + background_grid() +
   ggtitle("Pocillopora acuta") + 
